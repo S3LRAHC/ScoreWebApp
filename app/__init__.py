50573e1b492081fa8307f7dbe8f8c440ef28e4a8
IoT-Dashboard <iamguiilty@gmail.com>
2024-08-11 21:21:11 -0400

    Finished most submit form ui

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
-from app import pages
+from app import pages, database
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_prefixed_env()
+
+    database.init_app(app)
 
     app.register_blueprint(pages.bp)
+    print(f"Current Environment: {os.getenv('ENVIRONMENT')}")
+    print(f"Using Database: {app.config.get('DATABASE')}")
     return app
